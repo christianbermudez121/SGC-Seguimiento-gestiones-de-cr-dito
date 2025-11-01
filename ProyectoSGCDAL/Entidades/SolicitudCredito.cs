75f9bf4e060034552e47f55112f9c8271c137634
christianbermudez121 <110303421+christianbermudez121@users.noreply.github.com>
2025-11-01 00:16:24 -0600

    Agregar Repositorios Dtos Mapeo SolicitudCredito

---
+++ ProyectoSGCDAL/Entidades/SolicitudCredito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSGCDAL.Entidades
+{
+    public class SolicitudCredito
+    {
+        public int IdSolicitud { get; set; }
+        public string identificacion { get; set; } = string.Empty;
+        public int IdCliente { get; set; }
+        public decimal MontoSolicitado { get; set; }
+        public String comentarios { get; set; } = string.Empty;
+        public string Documento { get; set; } = string.Empty;
+        public DateTime FechaSolicitud { get; set; } = DateTime.Now;
+        public string Estado { get; set; } = "Registrado";
+    }
+}
---
+++ ProyectoSGCDAL/Repositorios/SolicitudCreditoRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using ProyectoSGCDAL.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSGCDAL.Repositorios
+{
+    public class SolicitudCreditoRepositorio : ISolicitudCreditoRepositorio
+    {
+        private List<SolicitudCredito> solicitudes = new List<SolicitudCredito>()
+            {
+            new SolicitudCredito
+            {
+                IdSolicitud = 1,
+                identificacion = "1234567890",
+                IdCliente = 1,
+                MontoSolicitado = 5000,
+                comentarios = "Necesito el crédito para comprar un vehículo.",
+                FechaSolicitud = DateTime.Now.AddDays(-10),
+                Estado = "Aprobado"
+            },
+            new SolicitudCredito
+            {
+                IdSolicitud = 2,
+                identificacion = "0987654321",
+                IdCliente = 2,
+                MontoSolicitado = 15000,
+                comentarios = "Crédito para remodelar mi casa.",
+                FechaSolicitud = DateTime.Now.AddDays(-5),
+                Estado = "Pendiente"
+            }
+        };
+
+
+
+
+    public async Task<bool> AgregarSolicitudAsync(SolicitudCredito solicitudcredito)
+        {
+            solicitudcredito.IdSolicitud = solicitudes.Any() ? solicitudes.Max(s => s.IdSolicitud) + 1 : 1;
+            return true;
+        }
+
+        public async Task<SolicitudCredito> ObtenerPorIdentificacionAsync(string identificacion)
+        {
+            var solicitud = solicitudes.FirstOrDefault(s => s.identificacion == identificacion);
+            return solicitud;
+        }
+    }
+}
